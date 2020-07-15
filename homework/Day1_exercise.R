e72556ef4dd9b5e6821e24d4440f9cd0af136527
Jihe Liu <31855167+jihe-liu@users.noreply.github.com>
2020-07-14 21:48:28 -0400

    Create Day1_exercise.R

---
+++ homework/Day1_exercise.R
@@ -0,0 +1,17 @@
+# 1. Cell counts: do ctrl and stim group have similar cell counts after filtering?
+# Yes
+
+# 2. UMI counts (transcripts) per cell: do you observe the removal of cells with less than 500 UMI?
+# Yes
+
+# 3. Genes detected per cell: do you observe the removal of cells with less than 250 genes?
+# Yes
+
+# 4. UMIs vs. genes detected: do you observe cells with a high number of UMIs but only a few number of genes?
+# No
+
+# 5. Mitochondrial counts ratio: do you observe the removal of cells with more than 0.2 mitochondrial counts ratio?
+# Yes
+
+# 6. Complexity: do you observe the removal of cells with less than 0.8 log10GenesPerUMI?
+# Yes
