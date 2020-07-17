97c2a446048ba26c722f547ffb59f6898529bed4
Jihe Liu <31855167+jihe-liu@users.noreply.github.com>
2020-07-17 17:08:03 -0400

    Update Day2_exercise_answer_key.R

--- homework/Day2_exercise_answer_key.R
+++ homework/Day2_exercise_answer_key.R
@@ -1,3 +1,20 @@
 #### Clustering
 # After loading seurat_integrated.RData, set the resolution to 0.4, and plot the UMAP. How many clusters are present in our data?
 # 13
+
+#### Clustering quality control
+# Hypothesize the clusters corresponding to each of the different clusters in the table:
+# Cell Type	Clusters
+# CD14+ monocytes	1, 3, 14
+# FCGR3A+ monocytes	9
+# Conventional dendritic cells	15
+# Plasmacytoid dendritic cells	19
+# Marcrophages	-
+# B cells	6, 11, 17
+# T cells	0, 2, 4, 5, 10, 13, 18
+# CD4+ T cells	0, 2, 4, 10, 18
+# CD8+ T cells	5, 13
+# NK cells	8, 12
+# Megakaryocytes	16
+# Erythrocytes -
+# Unknown	7, 20
