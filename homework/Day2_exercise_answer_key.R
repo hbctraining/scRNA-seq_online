d4185d7e21788b4404a576bf2d5ee42b2581b720
Jihe Liu <31855167+jihe-liu@users.noreply.github.com>
2020-07-20 16:54:04 -0400

    Update Day2_exercise_answer_key.R

--- homework/Day2_exercise_answer_key.R
+++ homework/Day2_exercise_answer_key.R
@@ -1,8 +1,9 @@
 #### Clustering
 # After loading seurat_integrated.RData, set the resolution to 0.4, and plot the UMAP. How many clusters are present in our data?
-# 13
+# Answer: 13
 
 #### Clustering quality control
+# Answer:
 # Hypothesize the clusters corresponding to each of the different clusters in the table:
 # Cell Type	Clusters
 # CD14+ monocytes	1, 3, 14
@@ -18,3 +19,7 @@
 # Megakaryocytes	16
 # Erythrocytes -
 # Unknown	7, 20
+
+#### Marker identification
+# In the previous lesson, we identified cluster 9 as FCGR3A+ monocytes by inspecting the expression of known cell markers FCGR3A and MS4A7. Use FindConservedMarkers() function to find conserved markers for cluster 9. What do you observe? Do you see FCGR3A and MS4A7 as highly expressed genes in cluster 9?
+# Answer: Yes, FCGR3A and MS4A7 are among the top highly expressed genes (2nd and 3rd in our analysis) when performing FindConservedMarkers() for cluster 9. This observation confirms cluster 9 as FCGR3A+ monocytes.
