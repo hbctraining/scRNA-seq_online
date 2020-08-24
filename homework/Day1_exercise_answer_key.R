ff701bab8c207839c52e41f6e4d6133f881d96f4
rkhetani <khetani.r@gmail.com>
2020-08-24 17:51:21 -0400

    Update Day1_exercise_answer_key.R

--- homework/Day1_exercise_answer_key.R
+++ homework/Day1_exercise_answer_key.R
@@ -1,3 +1,10 @@
+#### QC
+
+## Extract the new metadata from the filtered Seurat object to go through the same plots as with the unfiltered data
+
+# Save filtered subset to new metadata
+metadata_clean <- filtered_seurat@meta.data
+
 # 1. Cell counts: do ctrl and stim group have similar cell counts after filtering?
 # Yes
 
@@ -15,3 +22,13 @@
 
 # 6. Complexity: do you observe the removal of cells with less than 0.8 log10GenesPerUMI?
 # Yes
+
+#### scTransform
+
+# 1. Are the same assays available for the "stim" samples within the split_seurat object? What is the code you used to check that?
+# Yes they are
+split_seurat$stim@assays
+
+# 2. Any observations for the genes or features listed under "First 10 features:" and the "Top 10 variable features:" for "ctrl" versus "stim"?
+# "First 10 features:" - It appears that the same genes are present in both "ctrl" and "stim"
+# "Top 10 variable features:" - These are different in the the 2 conditions with some overlap between them
