ca49fbff6f3fcc440ad58c0ccb22a666ebe0306f
marypiper <marypiper@users.noreply.github.com>
2021-02-19 09:24:28 -0500

    Update integration_code.R

--- scripts/integration_code.R
+++ scripts/integration_code.R
@@ -1,1 +1,16 @@
+# Select the most variable features to use for integration
+integ_features <- SelectIntegrationFeatures(object.list = split_seurat, 
+                                            nfeatures = 3000) 
 
+# Prepare the SCT list object for integration
+split_seurat <- PrepSCTIntegration(object.list = split_seurat, 
+                                   anchor.features = integ_features)
+
+# Find best buddies - can take a while to run
+integ_anchors <- FindIntegrationAnchors(object.list = split_seurat, 
+                                        normalization.method = "SCT", 
+                                        anchor.features = integ_features)
+
+# Integrate across conditions
+seurat_integrated <- IntegrateData(anchorset = integ_anchors, 
+                                   normalization.method = "SCT")
