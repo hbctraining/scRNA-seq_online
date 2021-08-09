5acd110a1a672165f600990dbd5af78c11862e79
Meeta Mistry <meeta.mistry@gmail.com>
2021-08-09 14:59:36 -0400

    updating to reflect new questions

--- homework/Day1_exercise.R
+++ homework/Day1_exercise.R
@@ -1,25 +1,55 @@
 #### QC
 
-## Extract the new metadata from the filtered Seurat object to go through the same plots as with the unfiltered data
+## 1. Extract the new metadata from the filtered Seurat object using the code provided below:
 
 # Save filtered subset to new metadata
 metadata_clean <- filtered_seurat@meta.data
 
-## Perform all of the same plots as with the unfiltered data and answer the following questions:
-# 1. Cell counts: do ctrl and stim group have similar cell counts after filtering?
+## 2. Perform all of the same QC plots using the filtered data.
 
-# 2. UMI counts (transcripts) per cell: do you observe the removal of cells with less than 500 UMI?
 
-# 3. Genes detected per cell: do you observe the removal of cells with less than 250 genes?
+## 3. Report the number of cells left for each sample, and comment on whether the number of cells removed is high or low. Can you give reasons why this number is still not ~12K (which is how many cells were loaded for the experiment)?
 
-# 4. UMIs vs. genes detected: do you observe cells with a high number of UMIs but only a few number of genes?
 
-# 5. Mitochondrial counts ratio: do you observe the removal of cells with more than 0.2 mitochondrial counts ratio?
 
-# 6. Complexity: do you observe the removal of cells with less than 0.8 log10GenesPerUMI?
 
-#### scTransform
+## 4. After filtering for nGene per cell, you should still observe a small shoulder to the right of the main peak. What might this shoulder represent?
 
-# 1. Are the same assays available for the "stim" samples within the split_seurat object? What is the code you used to check that?
 
-# 2. Any observations for the genes or features listed under "First 10 features:" and the "Top 10 variable features:" for "ctrl" versus "stim"?
+
+
+## 5. When plotting the nGene against nUMI do you observe any data points in the bottom right quadrant of the plot? What can you say about these cells that have been removed?
+
+
+
+#### Exploring sources of unwanted variation
+
+## 1. First, turn the mitochondrial ratio variable into a new categorical variable based on quartiles (using the code below):
+
+# Check quartile values
+summary(seurat_phase@meta.data$mitoRatio)
+
+# Turn mitoRatio into categorical factor vector based on quartile values
+seurat_phase@meta.data$mitoFr <- cut(seurat_phase@meta.data$mitoRatio, 
+                   breaks=c(-Inf, 0.0144, 0.0199, 0.0267, Inf), 
+                   labels=c("Low","Medium","Medium high", "High"))
+
+## 2. Next, plot the PCA similar to how we did with cell cycle regression. Hint: use the new mitoFr variable to split cells and color them accordingly.
+
+
+## 3. 
+
+## a. Evaluate the PCA plot generated in #2.
+
+
+## b. Describe what you see.
+
+
+## c. Would you regress out mitochndrial fraction as a source of unwanted variation?
+
+
+#### SCTransform
+
+## 1. Are the same assays available for the "stim" samples within the split_seurat object? What is the code you used to check that?
+
+## 2. Any observations for the genes or features listed under "First 10 features:" and the "Top 10 variable features:" for "ctrl" versus "stim"?
