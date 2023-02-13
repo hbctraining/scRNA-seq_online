9f607b8c0db891266adaf1a125f1f1e804261902
Meeta Mistry <meeta.mistry@gmail.com>
2023-02-13 10:36:13 -0500

    updated to SCTv2

--- homework/Day2_exercise.R
+++ homework/Day2_exercise.R
@@ -3,13 +3,12 @@
 
 
 #### Clustering quality control
-# Hypothesize the clusters corresponding to each of the different clusters in the table (fill in the question marks):
 # Cell Type	Clusters
-# CD14+ monocytes	1, 3, 14
-# FCGR3A+ monocytes	9
-# Conventional dendritic cells	15
-# Plasmacytoid dendritic cells	19
-# Marcrophages	-
+# CD14+ monocytes	1, 3
+# FCGR3A+ monocytes	10
+# Conventional dendritic cells	14
+# Plasmacytoid dendritic cells	16
+# Macrophages	-
 # B cells	?
 # T cells	?
 # CD4+ T cells	?
@@ -20,4 +19,4 @@
 # Unknown	?
 
 #### Marker identification
-# In the previous lesson, we identified cluster 9 as FCGR3A+ monocytes by inspecting the expression of known cell markers FCGR3A and MS4A7. Use FindConservedMarkers() function to find conserved markers for cluster 9. What do you observe? Do you see FCGR3A and MS4A7 as highly expressed genes in cluster 9?
+# In the previous lesson, we identified cluster 10 as FCGR3A+ monocytes by inspecting the expression of known cell markers FCGR3A and MS4A7. Use FindConservedMarkers() function to find conserved markers for cluster 10. What do you observe? Do you see FCGR3A and MS4A7 as highly expressed genes in cluster 10?
